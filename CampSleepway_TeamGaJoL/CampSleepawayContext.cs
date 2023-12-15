cd8864bb881a289cea1a6373d029ace633c2a76d
jsirugo <jsirugo@yahoo.com>
2023-12-15 11:30:33 +0100

    Initial comit databas

--- CampSleepway_TeamGaJoL/CampSleepawayContext.cs
+++ CampSleepway_TeamGaJoL/CampSleepawayContext.cs
@@ -4,8 +4,27 @@ using Microsoft.Extensions.Logging;
 
 namespace CampSleepway_TeamGaJoL
 {
-    public class CampSleepawayContext :  DbContext
+    public class CampSleepawayContext :DbContext
     {
-        
+        public DbSet<Person> Persons { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            var connectionString = configuration.GetConnectionString("Local");
+
+            optionsBuilder.UseSqlServer(connectionString)
+                .LogTo(Console.WriteLine,
+                new[] { DbLoggerCategory.Database.Name },
+                LogLevel.Information)
+                .EnableSensitiveDataLogging();
+        }
     }
+
+
 }
