951e7cc18dc10542c65d8467cd710e7d5fabda53
jsirugo <jsirugo@yahoo.com>
2023-12-15 11:18:33 +0100

    js

--- CampSleepway_TeamGaJoL/Person.cs
+++ CampSleepway_TeamGaJoL/Person.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public required string FirstName { get; set; }  
         public required string LastName { get; set; }
-        public required string PhoneNumber { get; set; };
+        public required string PhoneNumber { get; set; }
 
 
 
