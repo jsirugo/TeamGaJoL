111789b8128fa8aa879a0a5e83d8750f1b6cd017
jsirugo <jsirugo@yahoo.com>
2023-12-15 11:17:44 +0100

    venne

---
+++ CampSleepway_TeamGaJoL/CampSleepawayContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace CampSleepway_TeamGaJoL
+{
+    public class CampSleepawayContext :  DbContext
+    {
+        
+    }
+}
---
+++ CampSleepway_TeamGaJoL/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace CampSleepway_TeamGaJoL
+{
+    public  class Person
+    {
+        public int Id { get; set; }
+        public required string FirstName { get; set; }  
+        public required string LastName { get; set; }
+        public required string PhoneNumber { get; set; };
+
+
+
+    }
+}
