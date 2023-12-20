37e9edb2d0d1962ba2e7c682f07ce9a2812b82c1
Lillebjörn Söderberg <lillebjorn.s@gmail.com>
2023-12-20 13:41:00 +0100

    Migrations commit

---
+++ CampSleepway_TeamGaJoL/Migrations/20231220123804_1.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CampSleepway_TeamGaJoL.Migrations
+{
+    /// <inheritdoc />
+    public partial class _1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cabins",
+                columns: table => new
+                {
+                    CabinId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cabins", x => x.CabinId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Sex = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Campers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    CounselorId = table.Column<int>(type: "int", nullable: false),
+                    StartDate = table.Column<int>(type: "int", nullable: false),
+                    EndDate = table.Column<int>(type: "int", nullable: false),
+                    CabinId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Campers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Campers_Cabins_CabinId",
+                        column: x => x.CabinId,
+                        principalTable: "Cabins",
+                        principalColumn: "CabinId");
+                    table.ForeignKey(
+                        name: "FK_Campers_Persons_Id",
+                        column: x => x.Id,
+                        principalTable: "Persons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Councelors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    CouncelorId = table.Column<int>(type: "int", nullable: false),
+                    CabinId = table.Column<int>(type: "int", nullable: false),
+                    PersonId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Councelors", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Councelors_Cabins_CabinId",
+                        column: x => x.CabinId,
+                        principalTable: "Cabins",
+                        principalColumn: "CabinId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Councelors_Persons_Id",
+                        column: x => x.Id,
+                        principalTable: "Persons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NextOfKins",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    NextOfKinId = table.Column<int>(type: "int", nullable: false),
+                    CamperId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NextOfKins", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_NextOfKins_Campers_CamperId",
+                        column: x => x.CamperId,
+                        principalTable: "Campers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_NextOfKins_Persons_Id",
+                        column: x => x.Id,
+                        principalTable: "Persons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Campers_CabinId",
+                table: "Campers",
+                column: "CabinId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Councelors_CabinId",
+                table: "Councelors",
+                column: "CabinId",
+                unique: true,
+                filter: "[CabinId] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NextOfKins_CamperId",
+                table: "NextOfKins",
+                column: "CamperId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Councelors");
+
+            migrationBuilder.DropTable(
+                name: "NextOfKins");
+
+            migrationBuilder.DropTable(
+                name: "Campers");
+
+            migrationBuilder.DropTable(
+                name: "Cabins");
+
+            migrationBuilder.DropTable(
+                name: "Persons");
+        }
+    }
+}
