a215bf88dc823f127c7461bedda97c3e9c5f62fb
Lillebjörn Söderberg <lillebjorn.s@gmail.com>
2023-12-22 11:39:55 +0100

    Made into one list and continued List Person

--- CampSleepway_TeamGaJoL/Program.cs
+++ CampSleepway_TeamGaJoL/Program.cs
@@ -1,9 +1,10 @@
-﻿using CampSleepway_TeamGaJoL;
+﻿using Azure;
+using CampSleepway_TeamGaJoL;
 using Microsoft.EntityFrameworkCore;
 
 using var context = new CampSleepawayContext();
 var filePath = Path.Combine(Directory.GetCurrentDirectory(), "Camperlist.csv");
-var persons = ReadCSV("Camperlist.csv");
+var persons = ReadCSV("People.csv");
 Console.WriteLine($"{persons.Count} rader hittade i CSV filen");
 //Kör enbart ifall personer inte är tillagda i eran list
 //foreach (var person in persons)
@@ -12,9 +13,11 @@ Console.WriteLine($"{persons.Count} rader hittade i CSV filen");
 //}
 //context.SaveChanges();
 //Console.WriteLine("Personer tillagda i listan");
+
+context.AddRange(persons);
+context.SaveChanges();
 static List<Person> ReadCSV(string filePath)
 {
-    
     var persons = new List<Person>();
 
     using var reader = new StreamReader(filePath);
@@ -23,7 +26,6 @@ static List<Person> ReadCSV(string filePath)
 
     while (!reader.EndOfStream)
     {
-        
         var line = reader.ReadLine();
         if (line == null)
         {
@@ -31,25 +33,75 @@ static List<Person> ReadCSV(string filePath)
         }
         var values = line.Split(',');
 
-        if (values.Length == 5)
+        if (values.Length == 8)
         {
             var firstName = values[0];
             var lastName = values[1];
             var phoneNumber = values[2];
             var sex = values[3];
             var age = int.Parse(values[4]);
+            var isCamper = int.Parse(values[5]) == 1;
+            var isCounselor = int.Parse(values[6]) == 1;
+            var isNextOfKin = int.Parse(values[7]) == 1;
 
-            var person = new Person
+            if (isCamper)
             {
-                
-                FirstName = firstName,
-                LastName = lastName,
-                PhoneNumber = phoneNumber,
-                Sex = sex,
-                Age = age
-            };
+                var camper = new Camper
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    PhoneNumber = phoneNumber,
+                    Sex = sex,
+                    Age = age,
+                    StartDate = DateTime.Now,
+                    EndDate = null,
+                    NextofKins = new List<NextOfKin>()
+                };
 
-            persons.Add(person); 
+                persons.Add(camper);
+            }
+            else if (isCounselor)
+            {
+                var counselor = new Councelor
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    PhoneNumber = phoneNumber,
+                    Sex = sex,
+                    Age = age,
+                    StartDate = DateTime.Now,
+                    EndDate = null,
+                    NextOfKins = new List<NextOfKin>()
+                };
+
+                persons.Add(counselor);
+            }
+            else if (isNextOfKin)
+            {
+                var nextOfKin = new NextOfKin
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    PhoneNumber = phoneNumber,
+                    Sex = sex,
+                    Age = age
+                };
+
+                persons.Add(nextOfKin);
+            }
+            else
+            {
+                var person = new Person
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    PhoneNumber = phoneNumber,
+                    Sex = sex,
+                    Age = age
+                };
+
+                persons.Add(person);
+            }
         }
     }
     return persons;
